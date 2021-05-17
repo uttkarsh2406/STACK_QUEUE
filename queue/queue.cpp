9dc9cbb24b69e8125525bc8981bdd077b6bc4921
uttkarsh2406 <74823692+uttkarsh2406@users.noreply.github.com>
2021-05-17 21:38:07 +0530

    Create queue.cpp

---
+++ queue/queue.cpp
@@ -0,0 +1,101 @@
+#include<iostream>
+using namespace std;
+
+class queue{
+    public:
+    int front;
+    int rear;
+    int size;
+    unsigned capacity;
+    int *arr;
+};
+
+queue *c_queue(unsigned size){
+    queue *que=new queue();
+    que->capacity=size;
+    que->front=0;
+    que->size=0;
+     
+    que->rear=size-1;
+    que->arr=new int[que->capacity];
+    return que;
+    
+}
+
+int is_ful(queue *que){
+    if(que->rear==que->capacity){
+        return 1;
+    }
+    else{
+        return 0;
+    }
+}
+
+int is_empt(queue *que){
+    if(que->size==0){
+        return 1;
+    }
+    else{
+        return 0;
+    }
+}
+
+void enqueue(queue *que,int data){
+    if(is_ful(que)){
+        return;
+    }
+    else{
+        que->rear=(que->rear+1);
+        que->arr[que->rear]=data;
+        que->size=que->size+1;
+
+    }
+}
+
+int dequeue(queue *que){
+    if(is_empt(que)){
+     exit(1);
+    }
+    else{
+        int item=que->arr[que->front];
+        que->front=que->front+1;
+        que->size--;
+        return item;
+    }
+}
+
+int front(queue *que){
+    if(is_empt(que)){
+        exit(1);
+    }
+    else{
+        int x=que->arr[que->front];
+        return x;
+    }
+}
+
+int rear(queue *que){
+    if(is_ful(que)){
+      exit(0);
+
+    }
+    else{
+        int x=que->arr[que->rear];
+        return x;
+    }
+
+}
+
+int main(){
+    int n;
+    cin>>n;
+    queue *que=c_queue(n);
+    char c;
+    int data;
+    cin>>c;
+    while(c!='e'){
+      if(c=='i'){
+          
+      }  
+    }
+}
