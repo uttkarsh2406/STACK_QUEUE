4147db6d06aa1cbfdd4ec9f0ec6ac76098e32d5d
uttkarsh2406 <74823692+uttkarsh2406@users.noreply.github.com>
2021-05-19 22:59:25 +0530

    Create evaluate_postfix.cpp

---
+++ problems/evaluate_postfix.cpp
@@ -0,0 +1,83 @@
+// { Driver Code Starts
+// C++ program to evaluate value of a postfix expression
+#include <stdio.h>
+#include <string.h>
+#include <ctype.h>
+#include <stdlib.h>
+#include <bits/stdc++.h>
+using namespace std;
+
+
+ // } Driver Code Ends
+
+
+
+class Solution
+{
+    public:
+    //Function to evaluate a postfix expression.
+    int evaluatePostfix(string s)
+    {
+        // Your code here
+        stack<int>a;
+        
+        for (int i = 0; i < s.length(); i++)
+        {
+            /* code */
+            if(s[i]==' '){
+                continue;
+
+            }
+            else if(isdigit(s[i])){
+                a.push(s[i]-'0');
+            }
+            else{
+                int n=a.top();
+                a.pop();
+                int m=a.top();
+                a.pop();
+
+                switch (s[i])
+                {
+                case /* constant-expression */'+':
+                    /* code */
+                     a.push(m+n);
+                    break;
+                case '-':
+                    a.push(m-n);
+                    break;
+                case '*':
+                    a.push(m*n);
+                    break;
+                case '/':
+                   a.push(m/n);
+                   break;
+
+                }
+            }
+        }
+        return a.top();
+        
+
+    }
+};
+
+// { Driver Code Starts.
+
+// Driver program to test above functions
+int main()
+{
+    int t;
+    cin>>t;
+    cin.ignore(INT_MAX, '\n');
+    while(t--)
+    {
+        string S;
+        cin>>S;
+        Solution obj;
+    
+    cout<<obj.evaluatePostfix(S)<<endl;
+    }
+    return 0;
+}
+  // } Driver Code Ends
