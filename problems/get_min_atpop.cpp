e37d66f1efbe91c7fcf857b0d6f05abc360768f4
uttkarsh2406 <74823692+uttkarsh2406@users.noreply.github.com>
2021-05-20 11:00:35 +0530

    Update get_min_atpop.cpp

--- problems/get_min_atpop.cpp
+++ problems/get_min_atpop.cpp
@@ -31,12 +31,39 @@ stack<int> _push(int arr[],int n)
 void _getMinAtPop(stack<int>s)
 {
     // your code here
-    stack<int>b;
+    std::vector<int>a;
     while (!s.empty())
     {
         /* code */
+        a.push_back(s.top());
+        s.pop();
         
     }
+    stack<int>min;
+    s.push(a.back());
+    min.push(a.back());
+
+    for (int i = a.size()-2; i >=0; i--)
+    {
+        /* code */
+        s.push(a[i]);
+        if(s.top()<min.top()){
+            min.push(s.top());
+        }
+        else{
+            min.push(min.top());
+        }
+
+    }
+    while (!s.empty())
+    {
+        /* code */
+        cout<<min.top()<<" ";
+        s.pop();
+        min.pop();
+    }
+    cout<<endl;
+    
     
 }
 
