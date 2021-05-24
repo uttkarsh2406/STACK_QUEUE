3d0d7b7a8a35ca2d67626758073de0a552244907
uttkarsh2406 <uttkarshraj789@gmail.com>
2021-05-24 12:48:43 +0530

    notanle to done

---
+++ problems/easy_string.cpp
@@ -0,0 +1,25 @@
+// { Driver Code Starts
+#include <bits/stdc++.h>
+using namespace std;
+string transform(string s);
+
+int main(){
+    int t;
+    cin >> t;
+    while(t--){
+        string s;
+        cin >> s;
+        cout << transform(s) << endl;
+    }
+return 0;
+}
+
+// } Driver Code Ends
+
+
+string transform(string S){
+    //complete the function here
+    
+
+}
+
