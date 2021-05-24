053ce71c48f0b52f8680380e048c2fb32f035c22
uttkarsh2406 <uttkarshraj789@gmail.com>
2021-05-24 13:00:10 +0530

    done

--- problems/easy_string.cpp
+++ problems/easy_string.cpp
@@ -19,7 +19,26 @@ return 0;
 
 string transform(string S){
     //complete the function here
+    string ans;
+    transform(S.begin(),S.end(),S.begin(),::tolower);
+    int c=1;
+    for (int i = 1; i < S.length(); i++)
+    {
+        /* code */
+        if(S[i]==S[i-1]){
+            c++;
+            continue;
+        }
+        else{
+            ans+=to_string(c);
+            ans+=S[i-1];
+            c=1;
+        }
+        
+    }
+    ans+=to_string(c);
+    ans+=S[S.length()-1];
+    return ans;
     
-
 }
 
