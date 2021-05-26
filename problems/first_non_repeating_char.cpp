5053ead2cb6b09888a1957244c8dcd3ea417b7f0
uttkarsh2406 <74823692+uttkarsh2406@users.noreply.github.com>
2021-05-26 22:27:48 +0530

    half_done first non repeating char

---
+++ problems/first_non_repeating_char.cpp
@@ -0,0 +1,29 @@
+// { Driver Code Starts
+#include<bits/stdc++.h>
+using namespace std;
+
+ // } Driver Code Ends
+class Solution {
+	public:
+		string FirstNonRepeating(string A){
+		    // Code here
+            hash<char>a;
+        
+
+		}
+
+};
+
+// { Driver Code Starts.
+int main(){
+	int tc;
+	cin >> tc;
+	while(tc--){
+		string A;
+		cin >> A;
+		Solution obj;
+		string ans = obj.FirstNonRepeating(A);
+		cout << ans << "\n";
+	}
+	return 0;
+}  // } Driver Code Ends
